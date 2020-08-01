43d59c693160756b2c0a93e0d54b6408c8e3966c
Leo Korbee <Leo.Korbee@xs4all.nl>
2020-08-01 23:16:35 +0200

    without serial debug output

--- src/main.cpp
+++ src/main.cpp
@@ -62,11 +62,13 @@
 
 
 void do_send(osjob_t* j);
+void disableUnusedGPIO(void);
+
 
 // for debugging redirect to hardware Serial2
 // Tx on PA2
-#define Serial Serial2
-HardwareSerial Serial2(USART2);   // or HardWareSerial Serial2 (PA3, PA2);
+//#define Serial Serial2
+//HardwareSerial Serial2(USART2);   // or HardWareSerial Serial2 (PA3, PA2);
 int i;
 
 
@@ -95,89 +97,90 @@ static osjob_t sendjob;
  };
 
  void onEvent (ev_t ev) {
-     Serial.print(os_getTime());
-     Serial.print(": ");
+  //   Serial.print(os_getTime());
+  //   Serial.print(": ");
      switch(ev) {
          case EV_SCAN_TIMEOUT:
-             Serial.println(F("EV_SCAN_TIMEOUT"));
+    //         Serial.println(F("EV_SCAN_TIMEOUT"));
              break;
          case EV_BEACON_FOUND:
-             Serial.println(F("EV_BEACON_FOUND"));
+    //         Serial.println(F("EV_BEACON_FOUND"));
              break;
          case EV_BEACON_MISSED:
-             Serial.println(F("EV_BEACON_MISSED"));
+    //         Serial.println(F("EV_BEACON_MISSED"));
              break;
          case EV_BEACON_TRACKED:
-             Serial.println(F("EV_BEACON_TRACKED"));
+    //         Serial.println(F("EV_BEACON_TRACKED"));
              break;
          case EV_JOINING:
-             Serial.println(F("EV_JOINING"));
+    //         Serial.println(F("EV_JOINING"));
              break;
          case EV_JOINED:
-             Serial.println(F("EV_JOINED"));
+    //         Serial.println(F("EV_JOINED"));
 
              // Disable link check validation (automatically enabled
              // during join, but not supported by TTN at this time).
              LMIC_setLinkCheckMode(0);
              break;
          case EV_RFU1:
-             Serial.println(F("EV_RFU1"));
+    //         Serial.println(F("EV_RFU1"));
              break;
          case EV_JOIN_FAILED:
-             Serial.println(F("EV_JOIN_FAILED"));
+    //         Serial.println(F("EV_JOIN_FAILED"));
              break;
          case EV_REJOIN_FAILED:
-             Serial.println(F("EV_REJOIN_FAILED"));
+    //         Serial.println(F("EV_REJOIN_FAILED"));
              break;
              break;
          case EV_TXCOMPLETE:
-             Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
+    //         Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
              if (LMIC.txrxFlags & TXRX_ACK)
-               Serial.println(F("Received ack"));
+    //           Serial.println(F("Received ack"));
              if (LMIC.dataLen)
              {
-               Serial.print(F("Received "));
-               Serial.print(LMIC.dataLen);
-               Serial.println(F(" bytes of payload"));
+    //           Serial.print(F("Received "));
+    //           Serial.print(LMIC.dataLen);
+    //           Serial.println(F(" bytes of payload"));
                // print data
                for (int i = 0; i < LMIC.dataLen; i++)
                {
-                 Serial.print(LMIC.frame[LMIC.dataBeg + i], HEX);
+    //             Serial.print(LMIC.frame[LMIC.dataBeg + i], HEX);
                }
-               Serial.println();
+    //           Serial.println();
              }
              // Schedule next transmission
              //os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(2), do_send);
 
              // Low power next transmission
-             Serial.println("going to sleep");
+    //         Serial.println("going to sleep");
 
              LowPower.begin(); // reset low power parameters, seems to work -> 0.6 uA without BME280
              delay(100);
              pinMode(PA6, INPUT_ANALOG);
-             LowPower.deepSleep(60000);
-  
-             Serial.println("queing next job");
+             // 5 minutes sleep
+             LowPower.deepSleep(300000);
+
+    //         Serial.println("queing next job");
              do_send(&sendjob);
              break;
          case EV_LOST_TSYNC:
-             Serial.println(F("EV_LOST_TSYNC"));
+    //         Serial.println(F("EV_LOST_TSYNC"));
              break;
          case EV_RESET:
-             Serial.println(F("EV_RESET"));
+      //       Serial.println(F("EV_RESET"));
              break;
          case EV_RXCOMPLETE:
              // data received in ping slot
-             Serial.println(F("EV_RXCOMPLETE"));
+    //         Serial.println(F("EV_RXCOMPLETE"));
              break;
          case EV_LINK_DEAD:
-             Serial.println(F("EV_LINK_DEAD"));
+    //         Serial.println(F("EV_LINK_DEAD"));
              break;
          case EV_LINK_ALIVE:
-             Serial.println(F("EV_LINK_ALIVE"));
+    //         Serial.println(F("EV_LINK_ALIVE"));
              break;
           default:
-             Serial.println(F("Unknown event"));
+    //         Serial.println(F("Unknown event"));
              break;
      }
  }
@@ -185,7 +188,7 @@ static osjob_t sendjob;
  void do_send(osjob_t* j){
      // Check if there is not a current TX/RX job running
      if (LMIC.opmode & OP_TXRXPEND) {
-         Serial.println(F("OP_TXRXPEND, not sending"));
+//         Serial.println(F("OP_TXRXPEND, not sending"));
      } else {
          // Prepare upstream data transmission at the next possible time.
          uint8_t dataLength = 8;
@@ -226,16 +229,16 @@ static osjob_t sendjob;
          bme.goToSleep();
 
          LMIC_setTxData2(1, data, sizeof(data), 0);
-         Serial.println(F("Packet queued"));
+  //       Serial.println(F("Packet queued"));
      }
      // Next TX is scheduled after TX_COMPLETE event.
  }
 
  void setup() {
-     Serial.begin(9600);
+  //   Serial.begin(9600);
 
      delay(5000);
-     Serial.println(F("Starting"));
+  //   Serial.println(F("Starting"));
 
      #ifdef VCC_ENABLE
      // For Pinoccio Scout boards
@@ -255,7 +258,7 @@ static osjob_t sendjob;
 
      // begin communication with BME280 and set to default sampling, iirc, and standby settings
      if (bme.begin() < 0) {
-       Serial.println("Error communicating with BME280 sensor, check wiring and SPI");
+    //   Serial.println("Error communicating with BME280 sensor, check wiring and SPI");
        while(1){}
      }
 
@@ -269,8 +272,8 @@ static osjob_t sendjob;
  void loop()
  {
     // check for sleeping
-    Serial.print("loop ");
-    Serial.println(i);
-    i++;
+    //Serial.print("loop ");
+    //Serial.println(i);
+    //i++;
     os_runloop_once();
  }
